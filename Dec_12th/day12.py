555ddd6adb580dcb51f337e05b314326ac2c3140
miguel <miguel@example.com>
2025-02-08 12:14:04 +0100

    day 12 abandoned temp

--- Dec_12th/day12.py
+++ Dec_12th/day12.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Set
 from data import data
+from collections import defaultdict
 
 class CharMatrix:
     UP = (-1, 0)
@@ -52,10 +53,100 @@ class Garden(CharMatrix):
                     plots_positions.append(n)  # Add neighbor to visited positions
                     stack.append(n)  # Push neighbor to the stack to visit later
 
-        return Region([Plot(pos, self) for pos in plots_positions])
+        return Region([Plot(pos, self) for pos in plots_positions], self)
 
     def total_fence_price(self) -> int:
         return sum([reg.fence_price() for reg in self.regions])
+
+    def total_fence_price_part2(self) -> int:
+        return sum([reg.fence_price_part2() for reg in self.regions])
+
+    def traverse_region_borders(self, region: "Region") -> int:
+        # border_plots = [plot for region.plots if plot.number_akin_neighbors==4] #doesnt work rn, id have to check all 8 neighbors
+        corners = 0
+        points_traversed_in_dirs = defaultdict(set)
+        # take one of the uppermost plots and move right to trace the external border
+        first_plot = sorted(region.plots, key=lambda plot: plot.point[0])[0]
+        ext_border_points, ext_border_corners, is_turn_clockwise = self._orbit_border(first_plot.point, start_dir_idx=0)
+        
+        assert is_turn_clockwise
+        interesting_points = set([plot.point for plot in region.plots if not plot.is_plot_below_same_type])
+        interesting_points = interesting_points - ext_border_points[2]
+
+        int_border_corners = 0
+        while interesting_points:
+            random_int_border_point = interesting_points.pop()
+            int_border_points, int_border_corners_i, is_turn_clockwise = self._orbit_border(random_int_border_point, start_dir_idx=2)
+            assert not is_turn_clockwise
+            int_border_corners += int_border_corners_i
+            interesting_points = interesting_points - int_border_points[2]
+
+            ## TODO look for points with a non-matching plot below that have not already been traversed in the left direction
+
+        corners = ext_border_corners + int_border_corners
+        return corners
+
+    def _orbit_border(self, start_point: Tuple[int, int], start_dir_idx = 0) -> Tuple[Dict[int, Set[Tuple[int, int]]], int, bool]:
+        assert start_dir_idx in [0, 1, 2, 3], "Unexpected direction index"
+        points_visited_in_directions = defaultdict(set)
+        clockwise_dir_cycle = [self.RIGHT, self.DOWN, self.LEFT, self.UP]
+        ref_val = self.value_at(start_point)
+        current_point = start_point
+        corners = 0
+        mov_dir_idx = start_dir_idx
+        loop = False
+        # turn direction counter: pos
+        turn_dir_count = [0, 0]
+        while not loop:
+            direct = mov_dir_idx
+            mov_dir_idx = (mov_dir_idx - 1) % 4 # starts by trying to move to relative left
+            isolated_point_counter = 0 # if it cant move in any direction, the region is 1 isolated point
+            # worst case, it returns to where it came from
+            while self.value_at(self.move(current_point, clockwise_dir_cycle[mov_dir_idx])) != ref_val and isolated_point_counter < 4:
+                mov_dir_idx = (mov_dir_idx + 1) % 4
+                isolated_point_counter += 1
+            # here, maybe, mark down the direction of travel in this position
+            if isolated_point_counter >= 4:
+                loop = True
+                corners += 4
+                turn_dir_count = [1, 1] # artificial, just so the turn is correctly identified as going clockwise
+            elif mov_dir_idx in points_visited_in_directions[current_point]:
+                loop = True
+            else:
+                turn_dir_count = self._turn_direction_counter(turn_dir_count, mov_dir_idx, current_point)
+                corners += self._number_of_corners_from_angle((mov_dir_idx - direct) % 4) # count corners
+                points_visited_in_directions[current_point].add(mov_dir_idx) # mark as visited
+                current_point = self.move(current_point, clockwise_dir_cycle[mov_dir_idx]) # move
+
+        is_turn_clockwise = any([count > 0 for count in turn_dir_count])
+        visited_points_with_dirs = {dr: set([pnt for pnt, dirs in points_visited_in_directions.items() if dr in dirs]) for dr in range(4)}
+        return tuple((visited_points_with_dirs, corners, is_turn_clockwise))
+        
+
+    def _turn_direction_counter(self, turn_dir_count: List[int], movement_direction: int, current_point: Tuple[int, int]) -> List[int]:
+        if movement_direction == 0: # right direction motion -> negative
+            turn_dir_count[0] -= current_point[0]
+        elif movement_direction == 2: # left direction motion -> positive
+            turn_dir_count[0] += current_point[0]
+        elif movement_direction == 1: # down direction motion -> positive
+            turn_dir_count[1] += current_point[1]
+        elif movement_direction == 3: # up direction motion -> negative
+            turn_dir_count[1] -= current_point[1]
+        else:
+            raise ValueError("Expected a number between 0 and 3")
+        return turn_dir_count
+
+
+    def _number_of_corners_from_angle(self, number_of_quarter_turns: int):
+        if number_of_quarter_turns == 0:
+            return 0
+        elif number_of_quarter_turns in [1, 3]:
+            return 1
+        elif number_of_quarter_turns == 2:
+            return 2
+        else:
+            raise ValueError("Expected a number between 0 and 3")
+
 
 class Plot:
     def __init__(self, point : Tuple[int, int], garden: Garden):
@@ -65,34 +156,29 @@ class Plot:
         self.number_akin_neighbors = sum([1 if self.value == garden.value_at(garden.move(self.point, dir)) else 0
                                           for dir in garden.directions])
 
-    def is_in_line(self, plot: "Plot") -> bool:
-        if plot.point[0] == self.point[0] or plot.point[1] == self.point[1]:
-            return True
-        return False
+    def is_plot_below_same_type(self) -> bool:
+        return self.value == self.garden.value_at(self.garden.move(self.point, self.garden.DOWN))
 
-    def is_side_plot(self) -> bool:
-        g = self.garden
-        # vertical side
-        if g.value_at(g.move(self.point, g.UP)) == self.value and g.value_at(g.move(self.point, g.DOWN)) == self.value:
-            return True
-        # horizontal side
-        if g.value_at(g.move(self.point, g.RIGHT)) == self.value and g.value_at(g.move(self.point, g.LEFT)) == self.value:
-            return True
-        return False
 
 class Region:
-    def __init__(self, plots: List[Plot]):
+    def __init__(self, plots: List[Plot], garden: Garden):
         self.plots = plots
+        self.garden = garden
+
+    def number_of_sides(self) -> int:
+        return self.garden.traverse_region_borders(self)
 
     def area(self) -> int:
         return len(self.plots)
 
     def perimeter(self) -> int:
-        return sum([(4 - plot.number_akin_neighbors) if not plot.is_side_plot() else 0
-                    for plot in self.plots])
+        return sum([(4 - plot.number_akin_neighbors) for plot in self.plots])
 
     def fence_price(self) -> int:
         return self.area() * self.perimeter()
+
+    def fence_price_part2(self) -> int:
+        return self.area() * self.number_of_sides()
 
 
 class Day12(TestCase):
@@ -102,6 +188,5 @@ class Day12(TestCase):
         print(f"Total price of fence {garden.total_fence_price()}")
 
     def test_day12_part2(self):
-        # just changed the perimeter method to discount plot in a side
         garden = Garden(data)
-        print(f"Total price of fence {garden.total_fence_price()}")
+        print(f"Total price of fence (counting sides) {garden.total_fence_price_part2()}")
